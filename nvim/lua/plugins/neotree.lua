12a965b23f0f6e3f71e8832bd14ade472fdfef7f
Paul Cross <me@paulcross.cz>
2023-11-28 22:59:50 +0100

    move neotree to left

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -3,14 +3,13 @@ return {
   opts = {
     sources = { "filesystem", "buffers", "git_status", "document_symbols" },
     open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
-    position = "right",
     filesystem = {
       bind_to_cwd = false,
       follow_current_file = { enabled = true },
       use_libuv_file_watcher = true,
     },
     window = {
-      position = "right",
+      position = "left",
       width = 50,
       mappings = {
         ["<space>"] = "none",
