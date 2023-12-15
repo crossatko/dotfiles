bcea0c744239f08207b56aedff1ae1764dae5654
Paul Cross <me@paulcross.cz>
2023-12-15 11:57:30 +0100

    cleanup

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,33 @@
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.blade = {
-  install_info = {
-    url = "https://github.com/EmranMR/tree-sitter-blade",
-    files = { "src/parser.c" },
-    branch = "main",
-  },
-  filetype = "blade",
-}
-
-local bladeGrp
-vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
-vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-  pattern = "*.blade.php",
-  group = bladeGrp,
-  callback = function()
-    vim.opt.filetype = "blade"
-  end,
-})
-
 return {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    "nvim-treesitter/nvim-treesitter-context",
+    -- "nvim-treesitter/nvim-treesitter-context",
     "vrischmann/tree-sitter-templ",
-    -- "windwp/nvim-ts-autotag",
+    "windwp/nvim-ts-autotag",
     "EmranMR/tree-sitter-blade",
+    "nvim-treesitter/nvim-treesitter-context",
   },
+  config = function()
+    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+    parser_config.blade = {
+      install_info = {
+        url = "https://github.com/EmranMR/tree-sitter-blade",
+        files = { "src/parser.c" },
+        branch = "main",
+      },
+      filetype = "blade",
+    }
 
+    local bladeGrp
+    vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
+    vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+      pattern = "*.blade.php",
+      group = bladeGrp,
+      callback = function()
+        vim.opt.filetype = "blade"
+      end,
+    })
+  end,
   opts = {
     ensure_installed = {
       "php",
@@ -59,12 +60,12 @@ return {
         script_element = "// %s",
       },
     },
-    -- autotag = {
-    --   enable = true,
-    --   enable_rename = true,
-    --   enable_close = true,
-    --   enable_close_on_slash = true,
-    -- },
+    autotag = {
+      enable = true,
+      enable_rename = true,
+      enable_close = true,
+      enable_close_on_slash = true,
+    },
     highlight = {
       enable = true,
       additional_vim_regex_highlighting = { "markdown" },
