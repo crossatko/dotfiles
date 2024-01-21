1eacff010377b2ea771a59c4531f5a20e68f4fae
Paul Cross <me@paulcross.cz>
2024-01-21 16:42:23 +0100

    add mute script

---
+++ waybar/scripts/easyeffects.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [ "$1" == "toggle" ]; then
+	# Toggle run toggle script
+	"$HOME"/.config/waybar/scripts/mute_script.sh
+fi
+
+# Set the preset file
+PRESET_FILE="$HOME/.easyeffects_preset"
+
+CURRENT_PRESET=$([ -e "$PRESET_FILE" ] && cat "$PRESET_FILE" || echo "mic")
+
+if [ "$CURRENT_PRESET" == "mic" ]; then
+	PERCENT="100"
+else
+	PERCENT="0"
+fi
+
+echo '{
+    "text": "'"$PERCENT%"'", 
+    "alt": "'"$CURRENT_PRESET"'", 
+    "percentage": "'"$PERCENT"'", 
+    "tooltip": "Click to '"$([ "$CURRENT_PRESET" == "mic" ] && echo "mute" || echo "unmute")"'", 
+    "class": "'"$CURRENT_PRESET"'"
+}' | jq --unbuffered --compact-output
---
+++ waybar/scripts/mute_script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Set the path to the EasyEffects program
+EASY_EFFECTS_PATH="easyeffects"
+
+# Set the preset file
+PRESET_FILE="$HOME/.easyeffects_preset"
+
+# Set the presets
+MIC_PRESET="mic"
+MUTE_PRESET="mute"
+
+# Check the current preset from the file or default to 'mic'
+if [ -e "$PRESET_FILE" ]; then
+	CURRENT_PRESET=$(cat "$PRESET_FILE")
+else
+	touch "$PRESET_FILE" && echo "$MIC_PRESET" >"$PRESET_FILE"
+	CURRENT_PRESET=$MIC_PRESET
+fi
+
+# Toggle between 'mic' and 'mute'
+if [ "$CURRENT_PRESET" == "$MIC_PRESET" ]; then
+	$EASY_EFFECTS_PATH -l $MUTE_PRESET
+	echo "$MUTE_PRESET" >"$PRESET_FILE"
+	echo "Switched to Mute preset"
+else
+	$EASY_EFFECTS_PATH -l $MIC_PRESET
+	echo "$MIC_PRESET" >"$PRESET_FILE"
+	echo "Switched to Mic preset"
+fi
