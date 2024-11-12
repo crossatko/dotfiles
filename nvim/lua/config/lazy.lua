bd9f4a3296974a75cb35c2173099125307754045
Paul Cross <me@paulcross.cz>
2024-11-12 11:01:50 +0100

    update nvim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -20,7 +20,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.ai.copilot-chat" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.coding.mini-surround" },
-    --    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
     { import = "plugins" },
   },
