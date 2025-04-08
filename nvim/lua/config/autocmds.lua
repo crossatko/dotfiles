12db8d622d30aed68251f6f5d261f880d4f5d051
Paul Cross <me@paulcross.cz>
2025-04-08 12:22:19 +0200

    add console log macro

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,5 +4,16 @@
 return {
   config = function()
     vim.cmd("hi Normal ctermbg=NONE guibg=NONE")
+
+    -- @l creates console log with selected text
+    local esc = vim.api.nvim_replace_termcodes("<Esc>", true, true, true)
+    vim.api.nvim_create_augroup("JSLogMacro", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = "JSLogMacro",
+      pattern = { "javascript", "typescript", "vue" },
+      callback = function()
+        vim.fn.setreg("l", "yoconsole.log('" .. esc .. "pa:" .. esc .. "la, " .. esc .. "pl")
+      end,
+    })
   end,
 }
