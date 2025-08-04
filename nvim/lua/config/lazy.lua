85e2f1b86e8886de18355d1f8be0158c162a39f1
Paul Cross <me@paulcross.cz>
2025-08-04 12:24:26 +0200

    idk what that was

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -53,4 +53,17 @@ require("lazy").setup({
       },
     },
   },
+  ui = {
+    -- a number <1 is a percentage., >1 is a fixed size
+    size = { width = 0.8, height = 0.8 },
+    wrap = true, -- wrap the lines in the ui
+    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+    border = "none",
+    -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+    backdrop = 100,
+    title = nil, ---@type string only works when border is not "none"
+    title_pos = "center", ---@type "center" | "left" | "right"
+    -- Show pills on top of the Lazy window
+    pills = true, ---@type boolean
+  },
 })
